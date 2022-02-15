8efda5c9df1b37153f0aafec7bebdd7899833789
Timothy Scheuering <timsch@cisco.com>
2022-02-15 11:51:15 -0500

    feat(models): export registry and request

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,12 @@
+import Registry from './Registry';
+import Request from './Request';
 import WebexCore from './WebexCore/WebexCore';
 
+export type { RequestMethod, RequestReceivingPayload, RequestSendingPayload } from './Request';
+export type { WebexCoreRegistries, WebexCoreSerial } from './WebexCore';
+
 export {
+  Registry,
+  Request,
   WebexCore,
 };
