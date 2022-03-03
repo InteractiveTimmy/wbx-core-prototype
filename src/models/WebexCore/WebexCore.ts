3211a28fe7bedea1c8d40c2bcbe0dd9b606b1d23
Timothy Scheuering <timsch@cisco.com>
2022-03-03 16:43:25 -0500

    refactor(models/webex-core): for new webex extension

--- src/models/WebexCore/WebexCore.ts
+++ src/models/WebexCore/WebexCore.ts
@@ -1,83 +1,25 @@
-import { ServicesRegistry } from '../../registries';
-
-import { WebexCoreRegistries, WebexCoreSerial } from './WebexCore.types';
-
 /**
  * WebexCore primary package model. This model reflects the entry point for this package.
  *
  * @beta
  */
-class WebexCore {
-  /**
-   * HTTP request tooling.
-   */
-  public request: unknown;
-
-  /**
-   * Logging tooling.
-   */
-  public logger: unknown;
-
-  /**
-   * Serializable registry data sets.
-   */
-  public registries: WebexCoreRegistries;
-
-  /**
-   * Web Socket tooling.
-   */
-  public socket: unknown;
-
+abstract class WebexCore<Serial> {
   /**
    * Construct a new WebexCore instance.
    *
    * @param serial - Serialized WebexCore instance.
    */
-  public constructor(serial?: unknown) {
-    this.registries = {};
-
+  public constructor(serial?: Serial) {
     if (serial) {
       this.deserialize(serial);
     }
   }
 
-  /**
-   * Deserialize a provided serial into this WebexCore instance.
-   *
-   * @param serial - Serial to use when mapping this WebexCore instance.
-   * @returns - This WebexCore instance.
-   */
-  public deserialize(serial: WebexCoreSerial): this {
-    this.registries.services = new ServicesRegistry(serial?.registries?.services);
+  public abstract deserialize(serial: Serial): this;
 
-    return this;
-  }
+  public abstract destroy(): void
 
-  public destroy(): void {
-    this.registries?.services?.destroy();
-  }
-
-  /**
-   * Initialize this WebexCore instance.
-   *
-   * @returns - This WebexCore instance.
-   */
-  public initialize(): Promise<this> {
-    return Promise.resolve(this);
-  }
-
-  /**
-   * Serialize this WebexCore instance.
-   *
-   * @returns - Serial of this WebexCore instance.
-   */
-  public serialize(): WebexCoreSerial {
-    return {
-      registries: {
-        services: this.registries.services.serialize(),
-      },
-    };
-  }
+  public abstract serialize(): Serial;
 }
 
 export default WebexCore;
--- src/models/WebexCore/index.ts
+++ src/models/WebexCore/index.ts
@@ -1,5 +1,3 @@
 import WebexCore from './WebexCore';
 
-export type { WebexCoreRegistries, WebexCoreSerial } from './WebexCore.types';
-
 export default WebexCore;
